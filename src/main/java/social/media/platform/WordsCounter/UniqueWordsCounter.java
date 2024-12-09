7bb22740c719fd851a0372b8fe64450d0f49a254
JulWas <julia.wasilewska.18@gmail.com>
2024-12-10 00:56:24 +0100

    commit hw8

---
+++ src/main/java/social/media/platform/WordsCounter/UniqueWordsCounter.java
@@ -0,0 +1,27 @@
+package social.media.platform.WordsCounter;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+public class UniqueWordsCounter {
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File("src/main/java/social/media/platform/WordsCounter/input.txt");
+        File outputFile = new File("src/main/java/social/media/platform/WordsCounter/output.txt");
+
+       String content = FileUtils.readFileToString(inputFile, "UTF-8");
+
+        Set<String> uniqueWords = Arrays.stream(StringUtils.split(content, " \n\r\t.,!?;: "))
+                .map(String::toLowerCase).collect(Collectors.toSet());
+        FileUtils.writeStringToFile(outputFile, " Number of unique words: " + uniqueWords.size(), "UTF-8" );
+
+        System.out.println("The result is written to a file: " + outputFile.getAbsolutePath());
+
+    }
+}
