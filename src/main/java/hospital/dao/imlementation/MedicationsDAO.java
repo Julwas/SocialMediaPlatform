30a676b5f1a50fce6cc371c58f4304455bfe6fed
JulWas <julia.wasilewska.18@gmail.com>
2025-02-03 22:31:11 +0100

    commit

---
+++ src/main/java/hospital/dao/imlementation/MedicationsDAO.java
@@ -0,0 +1,100 @@
+package hospital.dao.imlementation;
+
+import hospital.dao.AbstractDAO;
+import hospital.dao.IMedicationsDAO;
+import hospital.model.Medication;
+import hospital.model.Nurse;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MedicationsDAO extends AbstractDAO<Medication, Long> {
+    @Override
+    public void create(Medication medication) {
+        String sql = "INSERT INTO medication (name, description, manufacturer) VALUES (?, ?, ?)";
+        try (Connection connection = getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, medication.getName());
+            ps.setString(2, medication.getDescription());
+            ps.setString(3, medication.getManufacturer());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Optional<Medication> read(Long id) {
+        String sql = "SELECT * FROM medication WHERE id = ?";
+        try (Connection connection = getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Medication medication = new Medication(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getString("manufacturer")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(Medication medication) {
+        String sql = "UPDATE medication SET name = ?,description = ?, manufacturer = ? WHERE id = ?";
+        try (Connection connection = getConnection(); PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, medication.getName());
+            ps.setString(2, medication.getDescription());
+            ps.setString(3, medication.getManufacturer());
+            ps.setLong(4, medication.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        String sql = "DELETE FROM medication WHERE id = ?";
+        try (Connection connection = getConnection(); PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Medication> findAll() {
+        String sql = "SELECT * FROM medication";
+        List<Medication> medications = new ArrayList<>();
+
+        try (Connection connection = getConnection(); PreparedStatement ps = connection.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                if (rs.next()) {
+                    Medication medication= new Medication(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getString("description"),
+                            rs.getString("manufacturer")
+                    );
+                    medications.add(medication);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return medications;
+    }
+}
